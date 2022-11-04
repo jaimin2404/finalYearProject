aabcc6fc17ffaa3cf5dc84c7ead9d92db3145fc5
jaimin2404 <109670313+jaimin2404@users.noreply.github.com>
2022-11-04 12:18:58 +0530

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sidebar</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="sdbar">
+
+       <div class="icon">
+        <i class="fa-regular fa-clock"></i>
+        <a href="#">&nbsp;Recent</a>
+       </div>
+
+       <div class="icon">
+        <i class="fa-solid fa-baseball-bat-ball"></i>
+        <a href="#">Sports</a>
+       </div>
+
+       <div class="icon">
+        <i class="fa-solid fa-users-gear"></i>
+        <a href="#">Politics</a>
+       </div>
+     
+       <div class="icon">
+       <i class="fa-solid fa-business-time"></i>
+        <a href="#">Business</a>
+      </div> 
+
+
+      <div class="icon">
+        <i class="fa-solid fa-chart-line"></i>
+         <a href="#">&nbsp;Share market</a>
+       </div> <br>
+
+     <span class="category"><a href="#">More categories...</a></span>
+      
+       <!-- <div class="icon">
+        <i class="fa-solid fa-icons"></i>
+        <a href="#">Entertainment</a>
+       </div> -->
+    </div>
+</body>
+</html>
