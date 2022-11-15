1a95c7a1583160933603e13a97b06a46b5b06433
jaimin2404 <109670313+jaimin2404@users.noreply.github.com>
2022-11-15 09:07:33 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enews</title>
+    <!-- css -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- logo -->
+    <link rel="shortcut icon" href="./logo/logo_transparent.png" type="image/x-icon">
+    <!-- fontawesom -->
+    <script src="https://kit.fontawesome.com/96c97d736b.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    
+</body>
+</html>
