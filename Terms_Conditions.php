e096c9a39fee9978b615090c8cfca58ad77caff0
jaimin2404 <109670313+jaimin2404@users.noreply.github.com>
2022-11-15 21:14:46 +0530

    Add files via upload

---
+++ Terms_Conditions.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terms and Conditions</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="Style.css">
+</head>
+
+<body>
+    <div class="main">
+        <div class="heading">
+            <h1> Website Usage Terms And Conditions</h1>
+        </div>
+
+        <div class="container">
+            <p class="firstpara">
+                enews.co.in provides "The User" with access to its Site and the Services through its network of
+                websites. enews.co.in provides the Site and the Service to the user free of charge.
+                enews.co.in is not responsible for providing you access facilities or equipment (in any
+                form) to its service. You also comprehend and agree that enews.co.in makes no submission about the
+                suitability, reliability, availability, timeliness, and accuracy of the information, products, services
+                and related graphics. The Site and the Service is provided and available to your access "as is” without
+                warranty of any kind.
+                divyabhaskar.co.in hereby disclaims all warranties and conditions with regard to "The Site and the
+                Service”.
+            </p>
+            <p class="secpara">
+                <strong>USER CONDUCT :- </strong> "The Site and the Service" may include email services, message boards,
+                news groups, forums, communities
+                and/or other message or communication facilities designed to enable you to communicate with us. <br><br>
+
+                <strong>MODIFICATION TO " THE SERVICE " :- </strong> enews.co.in reserves the right to modify or
+                discontinue,
+                temporarily or permanently, 'The Service' (or any part of 'The Service') with or without notice to the
+                user at any time and from time to time. 'The User' agrees that enews.co.in shall not be liable to
+                the User or any third-party for any modification or discontinuance of the Service. <br><br>
+
+                <strong>USAGE OBLIGATIONS :-</strong> As a condition of your use of "The Site and the Service" you will
+                not use "The Site
+                and the Service" for any illegal purposes. You will be solely responsible for the contents of
+                transmissions made by you through "The Site and the Service". <br><br>
+
+                <strong>REGISTRATION OBLIGATIONS :-</strong> As a condition of your use of Tools for Communications you,
+                'The User' agree
+                to provide: (a) true, accurate, current and complete information about yourself as required by the Site
+                and the Service's registration form (such data being the "Registration Information ") (b) maintain and
+                promptly update the Registration Information to keep it accurate, current and complete. If you provide
+                any information that is untrue, inaccurate, not current or incomplete, divyabhaskar.co.in has the right
+                to terminate the user account and refuse any and all current or future use of 'The Site and the
+                Service'. (c) divyabhaskar.co.in has the right to use / disclose the aggregate registration information
+                to third parties in connection with marketing of services. <br><br>
+
+                <strong>ACKNOWLEDGMENT AND ACCEPTANCE OF TERMS OF SERVICE :-</strong> The Site and the Service is
+                provided to you under
+                this terms and conditions and any amendments thereto and any operating rules or policies that may be
+                published from time to time by enews.co.in as part of the terms of use. The enews.co.in
+                terms of use comprises the entire agreement between the User and divyabhaskar.co.in and supersedes any
+                prior agreements pertaining to the subject matter contained herein.
+            </p>
+
+            <p class="thirdpara">
+                <strong>You agree not to use "The Site and the Service" to:</strong><br> <br>
+
+
+                <strong>(1)</strong> Upload, post, email, transmit or otherwise make available any content that is
+                unlawful, damaging,
+                intimidating, harassing, defamatory, improper, obscene, vulgar, invasive of another's privacy, caste
+                related, ethnically or otherwise objectionable. <br> <br>
+
+                <strong>(2)</strong> "Stalk" or otherwise harass other users, collect or store personal data about other
+                users. <br> <br>
+
+                <strong>(3)</strong> Harm minors in any way. <br> <br>
+
+                <strong>(4)</strong> Forge headers or otherwise manipulate identifiers in order to disguise the origin
+                of any Content transmitted through the Site and the Services or develop restricted or password-only
+                access pages, or hidden pages or images (those not linked to from another accessible page. <br> <br>
+
+                <strong>(5)</strong> Transmit or otherwise make available any material in connection with surveys, chain
+                letters, junk email, spamming, contests, pyramid schemes, or any duplicative or unsolicited messages
+                (commercial or otherwise). <br> <br>
+
+                <strong>(6)</strong> Promote or provide instructional information about illegal activities, promote
+                physical harm or injury against any group or individual, or promote any act of cruelty to animals. This
+                may include, but is not limited to, providing instructions on how to assemble bombs, grenades and other
+                weapons, and creating "Crush" sites.<br> <br>
+
+              
+            </p>
+        </div>
+
+        <footer class="tandc_footer">
+            <a href="#"> <i class="fa-brands fa-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#"><i class="fa-brands fa-twitter"></i></a><br>
+            <strong>Copyright © 2022-23 Enews, All Rights Reserved</strong>
+          
+        </footer>
+    </div>
+</body>
+
+</html>
--- navbar.php
+++ navbar.php
@@ -1,18 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Enews</title>
-    <!-- css -->
-    <link rel="stylesheet" href="./css/style.css">
-    <!-- logo -->
-    <link rel="shortcut icon" href="./logo/logo_transparent.png" type="image/x-icon">
-    <!-- fontawesom -->
-    <script src="https://kit.fontawesome.com/96c97d736b.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="Style.css">
 </head>
+
 <body>
-    
+    <nav>
+        <div class="navbar">
+            <div class="logo">
+                <a href="">
+                    <img src="Logo/logo_transparent.png" alt="Enews">
+                    <span>Enews</span>
+                </a>
+            </div>
+            <div class="navitem">
+                <ul>
+                    <li>
+                        <a href=""><i class="fa-solid fa-house"></i><span>Home</span></a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa-solid fa-language"></i></a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa-solid fa-moon"></i></a>
+                    </li>
+                    <li class="searchbox">
+                        <input type="text" placeholder="Type to search">
+                    <a href=""><i class="fa-solid fa-magnifying-glass"></i></a> 
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
 </body>
+
 </html>
