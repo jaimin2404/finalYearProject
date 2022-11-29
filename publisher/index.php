5afb6b7ca77399fdffa0428a8d31674e22d16994
jaimin2404 <109670313+jaimin2404@users.noreply.github.com>
2022-11-29 08:47:47 +0530

    Add files via upload

---
+++ publisher/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enews</title>
+    <!-- css -->
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- logo -->
+    <link rel="shortcut icon" href="../logo/logo_transparent.png" type="image/x-icon">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <script src="../script/script.js"></script>
+    <!-- fontawesom -->
+    <script src="https://kit.fontawesome.com/96c97d736b.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="login publisher-login">
+        <div class="publisher">
+            <div class="form">
+                <div class="login-logo h-1">PUBLISHER LOGIN</div>
+                <form action="" autocomplete="off">
+                    <div class="input">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" id="username" placeholder="Username">
+                    </div>
+                    <div class="input">
+                        <label for="password">Password</label>
+                        <input type="text" name="password" id="password" placeholder="Password">
+                    </div>
+                    <div class="input submit">
+                        <button type="submit" class="btn"> <i class="fas fa-sign-in-alt"></i> Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
